44aed3467c700d5a5a17a35fae1685b53f224009
Jeff Risberg <jrisberg@aol.com>
2014-08-21 01:56:02 +0000

    added dependency on ejs helpers

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -11,8 +11,10 @@
 
 module.exports.bootstrap = function(cb) {
 
+  require('express-helpers')(sails.hooks.http.app);
+
   // It's very important to trigger this callback method when you are finished
-  // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+  // with the bootstrap, or your server will never start.
   Activity.destroy({}, function confirm(err,x) {
     Activity.create({name: "Hiking", type: "Physical", points: 5}, 
       function confirm(err, activity) {
