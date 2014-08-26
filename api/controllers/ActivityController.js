a829cdfc9f4a62706fa602f613e30c5503272072
Jeff Risberg <jrisberg@aol.com>
2014-08-26 00:47:14 +0000

    added CRUD code from SA03

--- api/controllers/ActivityController.js
+++ api/controllers/ActivityController.js
@@ -1,35 +1,76 @@
 module.exports = {
     index: function (req, res) {      
-        Activity.find({}).exec(function(err, activities) {
-            if (err) {
-                sails.log.err("-----------err", err);
-                res.send(400);
-                return;
-            }
+      Activity.find({}).exec(function(err, activities) {
+        if (err) {
+          sails.log.err("-----------err", err);
+          res.send(400);
+          return;
+        }
 
-            var data = {activities: activities};
-
-            res.view(data);
-        });
+        res.view({activities: activities});
+      });
     },
 
     view: function (req, res) {
-        var id = req.param("id", null);
+      var id = req.param("id", null);
 
-        Activity.findOne(id, function(err, activity) {
-            var data = { activity: activity };
+      Activity.findOne(id, function(err, activity) {
+        if (activity === undefined) return res.notFound();
 
-            console.log("found the activity " + activity);
-            if (activity === undefined) return res.notFound();
-            res.view(data);
-        });
+        res.view({activity: activity});
+      });
     },
 
     create: function (req, res) {
-        Activity.create({name: "Dog Walking", type: "Physical", points: 5}, 
-          function(err, activity) {
+      if (req.method == "POST" && req.param("Activity", null) != null) {
+        Activity.create(req.param("Activity"), function (err, model) {
 
-            res.redirect("/activity");
+          if (err) {
+            res.send("Error something went wrong");
+          } else {
+            res.redirect("/activity/index");
+          } 
         });
+      } else {
+        res.render('activity/create');
+      }
+    },
+
+    update: function (req, res) {
+      var id = req.param('id', null);
+    
+      Activity.findOne(id, function(err, activity) {
+        if (activity === undefined) return res.notFound();
+
+        if (req.method = "POST" && req.param('Activity', null) != null) {
+          var p = req.param('Activity', null);
+                      
+          activity.name = p.name;
+          activity.type = p.type;
+          activity.points = p.points;
+                  
+          activity.save(function (err) {                    
+            if (err) {
+              res.send("Error");
+            } else {                      
+              res.redirect("/activity/view/" + activity.id);
+            }                                        
+          });               
+        } else {                  
+          res.render('activity/update', {activity: activity});
+        }                
+      });           
+    },
+
+    delete: function (req, res) {
+      var id = req.param('id', null);
+
+      Activity.findOne(id, function(err, activity) {
+        if (activity === undefined) return res.notFound();
+
+        activity.destroy(function (error) {
+          res.redirect('/activity/index');       
+        });
+      });          
     }     
 };
