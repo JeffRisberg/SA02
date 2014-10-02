bc21278b38a43cbc1fa7911aecd0ffc5dda0c9ca
Jeff Risberg <jrisberg@aol.com>
2014-10-02 07:33:47 +0000

    took dummy data out of bootstrap

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -15,31 +15,6 @@ module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap, or your server will never start.
-  Activity.destroy({}, function confirm(err,x) {
-    Activity.create({name: "Hiking", type: "Physical", points: 5}, 
-      function confirm(err, activity) {
-        console.log(err);
-        if (err) {
-          cb(err);
-          return;
-        }
-        else {
-          console.log('created: ', activity);
-        }
-    });
-
-    Activity.create({name: "Walking", type: "Physical", points: 1},
-      function confirm(err, activity) {
-        console.log(err);
-        if (err) {
-          cb(err);
-          return;
-        }
-        else {
-          console.log('created: ', activity);
-        }
-    });
-  });
 
   cb();
 };
